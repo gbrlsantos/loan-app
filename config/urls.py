3671b887191c1da7bb133d6f4d0bb18433584fde
Gabriela Cruz <gabrielasnts30@gmail.com>
2022-07-05 20:43:06 -0300

    url to api

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from loan.views.ratetables_view import RateTablesViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'ratetables', RateTablesViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
