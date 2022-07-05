3023f709cc1c0a3afa4a578eeee96fbda794d5f8
Gabriela Cruz <gabrielasnts30@gmail.com>
2022-07-05 20:45:41 -0300

    many to many field serializer

--- loan/serializers/rate_table_serializer.py
+++ loan/serializers/rate_table_serializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from loan.models.rate_table import RateTable
+from loan.serializers.installment_serializer import InstallmentSerializer
 
 class RateTableSerializer(serializers.ModelSerializer):
+  installments = InstallmentSerializer(read_only=True, many=True)
   class Meta:
     model = RateTable
-    fields = '__all__'
+    fields = ('name', 'installments')
